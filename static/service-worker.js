10a725be99ae98570beef2136f195397891dc654
Pranesh17-s <praneshpalamedu17@gmail.com>
2024-10-15 10:46:40 +0530

    Add files via upload

---
+++ static/service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'emotion-detector-cache-v3';
+const urlsToCache = [
+    '/',
+    '/static/style.css',
+    '/static/script.js',
+    '/static/icon.png',
+    '/models/roberta_sentiment.onnx',  // Cache the ONNX model for offline usage
+    '/manifest.json'
+];
+
+// Install event to cache resources
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Fetch event to serve cached resources
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Activate event to clean up old caches
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
